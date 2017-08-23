09d75900439507f8405d7f2dd8e41903358ffbf9
dziedada <dziedada@umich.edu>
2017-08-22 22:03:42 -0400

    Committing miscelaneous progress.

--- src/deps/Obstacle.hpp
+++ src/deps/Obstacle.hpp
@@ -1,3 +1,6 @@
+#ifndef _OBSTACLE_HPP_
+#define _OBSTACLE_HPP_
+
 #include "Entity.hpp"
 #include "Circle.hpp"
 #include "Rectangle.hpp"
@@ -20,3 +23,5 @@ private:
 	Circle roombaBody;
 	Circle pillar;
 };
+
+#endif
